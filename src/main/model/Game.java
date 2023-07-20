fd23d06ac2110a240c8ba6991cbc1afaffcd345f
sabrinawoo <sabrinawoo3895@gmail.com>
2023-07-19 17:58:35 -0700

    Phase 1 attempt to fix line coverage :(

--- src/main/model/Game.java
+++ src/main/model/Game.java
@@ -37,8 +37,8 @@ public class Game {
 
     //EFFECTS: updates the fruit and enemies
     public void tick() {
-        moveFruit(fallingFruit);
-        moveEnemies(fallingEnemies);
+        moveFruit(this.fallingFruit);
+        moveEnemies(this.fallingEnemies);
         createFruit();
         createEnemies();
         handleEnemies();
